ea6f3e6f4ba90e7b60e5d4286eff188d67c47d2f
Juanes Velez <juan.velez@academlo.com>
2023-03-04 08:50:30 -0500

    fix clock

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,27 +1,31 @@
 import React, { useEffect, useState } from 'react';
 
 const Clock = () => {
-  const [time, setTime] = useState(new Date());
-  const [idTimer, setIdTimer] = useState(null);
+  const [time, setTime] = useState(null);
+  const [isActive, setIsActive] = useState(true);
 
-  const stopTime = () => {
-    clearInterval(idTimer);
-  };
+  useEffect(() => setTime(new Date()), []);
 
   useEffect(() => {
-    const id = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
+    let id;
+    if (isActive) id = setTimeout(() => setTime(new Date()), 1000);
 
-    console.log({ id });
-    setIdTimer(id);
-  }, []);
+    return () => {
+      if (id) clearTimeout(id);
+    };
+  }, [time, isActive]);
 
   return (
     <div>
-      <p className="text-2xl text-yellow-400">{time.toString()}</p>
-      <button onClick={stopTime} className="text-white text-xl bg-emerald-500 px-3 py-2">
-        Stop Time
+      <p className="text-2xl text-yellow-400">{time?.toString()}</p>
+      <button
+        onClick={() => setIsActive(!isActive)}
+        className={
+          'block text-white text-xl font-bold px-3 py-2 mx-auto mt-5 rounded-md' +
+          (isActive ? ' bg-red-400' : ' bg-emerald-500')
+        }
+      >
+        {isActive ? 'Stop Time' : 'Activate Time'}
       </button>
     </div>
   );
